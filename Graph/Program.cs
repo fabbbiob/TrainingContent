13f5fb825c9baccc1ee7ce1786f7ae8f441ec9cb
fabio bongiovanni <fabio.bongiovanni@live.com>
2020-03-26 08:59:31 +0100

    graph user and group

---
+++ Graph/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security;
+using Microsoft.Identity.Client;
+using Microsoft.Graph;
+using Microsoft.Extensions.Configuration;
+using Helpers;
+
+namespace graphgroups01
+{
+  class Program
+  {
+    static void TheMain(string[] args)
+    { }
+
+    private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config)
+    {
+      var tenantId      = config["tenantId"];
+      var clientId      = config["applicationId"];
+      var clientSecret  = config["applicationSecret"];
+      var authority     = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
+
+      List<string> scopes = new List<string>();
+      //scopes.Add(clientId + "/.default");
+      scopes.Add("https://graph.microsoft.com/.default");
+      
+
+      var cca = ConfidentialClientApplicationBuilder.Create(clientId)
+                                              .WithAuthority(authority)
+                                              .WithClientSecret(clientSecret)
+                                              .Build();
+      return MsalAuthenticationProvider.GetInstance(cca, scopes.ToArray());
+    }
+
+    private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config)
+    {
+      var authenticationProvider = CreateAuthorizationProvider(config);
+      var graphClient = new GraphServiceClient(authenticationProvider);
+      return graphClient;
+    }
+
+    private static IConfigurationRoot LoadAppSettings()
+    {
+      try
+      {
+        var config = new ConfigurationBuilder()
+                          .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json", false, true)
+                          .Build();
+
+        if (string.IsNullOrEmpty(config["applicationId"]) ||
+            string.IsNullOrEmpty(config["applicationSecret"]) ||
+            string.IsNullOrEmpty(config["tenantId"]) ||
+            string.IsNullOrEmpty(config["targetUserId"]))
+        {
+          return null;
+        }
+
+        return config;
+      }
+      catch (System.IO.FileNotFoundException)
+      {
+        return null;
+      }
+    }
+  }
+}
